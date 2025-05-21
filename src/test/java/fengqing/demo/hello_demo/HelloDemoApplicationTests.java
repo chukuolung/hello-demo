d9dd903262bceff1fdf614c1108796f23844bec7
chukuolung <chukuo99@gmail.com>
2025-05-21 16:51:32 +0800

    init

---
+++ src/test/java/fengqing/demo/hello_demo/HelloDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fengqing.demo.hello_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
